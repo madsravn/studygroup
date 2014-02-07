dcb84286984c2b189dfd55ef377835dbe74f7936
Mads Ravn <madsravn@gmail.com>
2014-02-07 15:49:18 +0100

    10 spp

--- miro/Source/Scene.cpp
+++ miro/Source/Scene.cpp
@@ -14,7 +14,7 @@
 Scene * g_scene = 0;
 const int recDepth = 5;
 const int pathBounces = 5;
-const int pathSamples = 128;
+const int pathSamples = 10;
 
 Vector3 Scene::getHDRColorFromVector(const Vector3 &direction) const {
 
