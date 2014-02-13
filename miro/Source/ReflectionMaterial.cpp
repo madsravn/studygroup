691855c6f8369d4d07ea1060bd3d3c48110704cf
Mads Ravn <madsravn@gmail.com>
2014-02-13 11:39:26 +0100

    New errors

--- miro/Source/ReflectionMaterial.cpp
+++ miro/Source/ReflectionMaterial.cpp
@@ -23,7 +23,7 @@ Vector3 ReflectionMaterial::shade(const Ray& ray, const HitInfo& hit, const Scen
 	}
 };
 
-Vector3 ReflectionMaterial::shade(const std::vector<HitInfo> path, const int pathPosition, const Scene& scene, bool log) const {
+Vector3 ReflectionMaterial::shade(const std::vector<HitInfo>& path, const int pathPosition, const Scene& scene, bool log) const {
 	if(pathPosition + 1 > path.size()) { // Not last element
 		return path.at(pathPosition + 1).material->shade(path, pathPosition + 1, scene, log);	
 	}
--- miro/Source/RefractionMaterial.cpp
+++ miro/Source/RefractionMaterial.cpp
@@ -47,10 +47,10 @@ Vector3 RefractionMaterial::shade(const Ray& ray, const HitInfo& hit, const Scen
 	return refractionColor;
 }
 //
-Vector3 RefractionMaterial::shade(const std::vector<HitInfo> path, const int pathPosition, const Scene& scene, bool log = false) const {
+Vector3 RefractionMaterial::shade(const std::vector<HitInfo>& path, const int pathPosition, const Scene& scene, bool log) const {
 
-	if(pathPosition + 1 > path.size) { // Not last element
-		return path.hits[pathPosition + 1].material->shade(path, pathPosition + 1, scene, log);	
+	if(pathPosition + 1 > path.size()) { // Not last element
+		return path.at(pathPosition + 1).material->shade(path, pathPosition + 1, scene, log);	
 	}
 	return Vector3(0,0,0);
 }
